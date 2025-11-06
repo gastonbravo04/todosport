d30e637922d2a4b49868476cf9112850f9a23d19
gastonbravo04 <mg.bravo@alumno.um.edu.ar>
2025-11-06 17:15:43 -0300

    Habilite CORS para todos los orígenes y actualice la lista de orígenes permitidos.

--- django_api/settings.py
+++ django_api/settings.py
@@ -190,6 +190,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS: list[str] = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
